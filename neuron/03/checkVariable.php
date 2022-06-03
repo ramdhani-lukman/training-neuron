6943cb0ef8800f64346e510bffa4fee7ca8beaac
ramdhani <ramdhani@neuronworks.co.id>
2022-06-03 16:51:08 +0700

    check variable

---
+++ neuron/03/checkVariable.php
@@ -0,0 +1,22 @@
+<?php
+$var1 = 10;
+if(isset($var1)){
+    echo "true";
+}else{
+    echo "false";
+}
+
+echo '<br>---------------------------------------<br>';
+
+$startDate = '';
+$endDate;
+
+if($startDate != ''){
+    /* Your code here */
+}
+
+if(!empty($startDate)){
+    echo "Tanggal memiliki value";
+}else{
+    echo "Tanggal tidak memiliki value";
+}
